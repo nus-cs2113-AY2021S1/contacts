c008662bf8b8eae53ef8ba85cb134ed6ead23f76
DESKTOP-7Q3MLGS\gunit <gunitmittal@gmail.com>
2020-08-28 15:09:03 +0530

    expected text changed

--- src/main/java/Contacts1.java
+++ src/main/java/Contacts1.java
@@ -593,7 +593,7 @@ public class Contacts1 {
      * @param phone to be validated
      */
     private static boolean isValidPhone(String phone) {
-        return !phone.isEmpty();
+        return !phone.isEmpty() && phone.length()==8;
         //TODO: implement a better validation
     }
 
